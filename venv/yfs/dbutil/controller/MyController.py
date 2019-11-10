d64a9165fac239fb7e49f4d4b99921bdc2ed07d2
yangfengshuai <yangfengshuai@xiangshang360.com>
2019-11-10 22:32:38 +0800

    页面功能进一步完善

--- venv/yfs/dbutil/controller/MyController.py
+++ venv/yfs/dbutil/controller/MyController.py
@@ -3,52 +3,86 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 import json
+import decimal
 
 from datetime import date, datetime
 
 import yfs.dbutil.service.MyService as myService
 
 
-class DateEncoder(json.JSONEncoder):
+class MyEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, datetime):
             return o.strftime('%Y-%m-%d %H:%M:%S')
         elif isinstance(o, date):
             return o.strftime('%Y-%m-%d')
+        elif isinstance(o, decimal.Decimal):
+            return float(o)
         else:
             return json.JSONEncoder.default(self, o)
 
 
-@app.route('/dbIndex', methods=['GET', 'POST'])
+
+@app.route('/index', methods=['GET', 'POST'])
 def db_index():
+    database = request.args.get("database")
     return render_template('index.html')
 
 
 @app.route('/dbExecute', methods=['GET', 'POST'])
 def db_execute():
     sqlContent = request.form.get("sqlContent")
+    database = request.form.get("database")
     if sqlContent.lstrip().startswith('select'):
-        sqlColumnAndData = myService.selectExecute(sqlContent)
-        resultDict = {"column": sqlColumnAndData[0], "dataList": sqlColumnAndData[1], "type": "query"}
-        return json.dumps(resultDict, cls=DateEncoder)
+        sqlColumnAndData = myService.selectExecute(database, sqlContent)
+        result_dict = {"column": sqlColumnAndData[0], "dataList": sqlColumnAndData[1], "type": "query",
+                       "tableName": sqlColumnAndData[2].pop()}
+        return json.dumps(result_dict, cls=MyEncoder)
     else:
-        updateCount = myService.update_execute(sqlContent)
-        resultDict = {"updateCount": updateCount, "type": "update"}
-        return json.dumps(resultDict, cls=DateEncoder)
+        update_count = myService.update_execute(database, sqlContent)
+        result_dict = {"updateCount": update_count, "type": "update"}
+        return json.dumps(result_dict, cls=MyEncoder)
 
 
 @app.route('/queryTables', methods=['GET', 'POST'])
 def query_tables():
     table_name = request.form.get("tableName")
-    tables = myService.query_tables(table_name)
-    return json.dumps(tables, cls=DateEncoder)
+    database = request.form.get("database")
+    tables = myService.query_tables(database, table_name)
+    return json.dumps(tables, cls=MyEncoder)
 
 
 @app.route('/queryColumns', methods=['GET', 'POST'])
 def query_columns():
     table_name = request.form.get("tableName")
-    tables = myService.query_columns(table_name)
-    return json.dumps(tables, cls=DateEncoder)
+    database = request.form.get("database")
+    tables = myService.query_columns(database,table_name)
+    return json.dumps(tables, cls=MyEncoder)
+
+
+@app.route('/updateColumn', methods=['GET', 'POST'])
+def update_column():
+    table_name = request.form.get("tableName")
+    column_name = request.form.get("columnName")
+    column_value = request.form.get("columnValue")
+    primary_key = request.form.get("primaryKey")
+    primary_value = request.form.get("primaryValue")
+    database = request.form.get("database")
+    update_count = myService.update_column(database, table_name, column_name, column_value, primary_key, primary_value)
+    result_dict = {"updateCount": update_count, "type": "update_column"}
+    return json.dumps(result_dict, cls=MyEncoder)
+
+
+
+@app.route('/delById', methods=['GET', 'POST'])
+def del_by_id():
+    database = request.form.get("database")
+    table_name = request.form.get("tableName")
+    id = request.form.get("id")
+
+    update_count = myService.del_by_id(database, table_name, id)
+    result_dict = {"updateCount": update_count, "type": "del_by_id"}
+    return json.dumps(result_dict, cls=MyEncoder)
 
 
 if __name__ == '__main__':
--- venv/yfs/dbutil/service/MyService.py
+++ venv/yfs/dbutil/service/MyService.py
@@ -1,26 +1,51 @@
 import pymysql
 import time
+import ply.lex as lex, re
+import json
+
 
 # mysql数据库属性
-host = '192.168.230.138'
-port = 3306
-username = 'root'
-password = '123456'
-database = 'yfs'
-def getDb():
-    db = pymysql.connect(
-        host=host,
-        user=username,
-        passwd=password,
-        db=database,
-        port=port,
-        charset='utf8',
-        cursorclass=pymysql.cursors.DictCursor)
-    return db
+# host = '192.168.230.138'
+# port = 3306
+# username = 'root'
+# password = '123456'
+# database = 'yfs'
+# def getDb():
+#     db = pymysql.connect(
+#         host=host,
+#         user=username,
+#         passwd=password,
+#         db=database,
+#         port=port,
+#         charset='utf8',
+#         cursorclass=pymysql.cursors.DictCursor)
+#     return db
 
 
-def selectExecute(sql):
-    db = getDb()
+def __get_db(database):
+    "获取数据库连接，从本地文件中获取配置信息，放置数据库配置在代码中出现"
+    f = open("D:\py_database_key.txt", "r")
+    database_keys_str = f.read()
+    database_keys = database_keys_str.split("\n")
+    for database_key_val in database_keys:
+        database_key = database_key_val.split("=")[0]
+        if database == database_key:
+            # 根据配置中的key获取本地文件中的数据，类型是json格式
+            database_val_json = database_key_val.split("=")[1]
+            database_dict = json.loads(database_val_json)
+            db = pymysql.connect(
+                host=database_dict["host"],
+                user=database_dict["username"],
+                passwd=database_dict["password"],
+                db=database_dict["database"],
+                port=database_dict["port"],
+                charset='utf8',
+                cursorclass=pymysql.cursors.DictCursor)
+            return db
+
+
+def selectExecute(database, sql):
+    db = __get_db(database)
     cursor = db.cursor()
     cursor.execute(sql)
     dataList = cursor.fetchall()
@@ -28,11 +53,11 @@ def selectExecute(sql):
     if dataList:
         for (k, v) in dataList[0].items():
             columnNameList.append(k)
-    return columnNameList, dataList
+    return (columnNameList, dataList, extract_table_name_from_sql(sql))
 
 
-def query_tables(table_name):
-    db = getDb()
+def query_tables(database, table_name):
+    db = __get_db(database)
     cursor = db.cursor()
     cursor.execute("show tables")
     data_list = cursor.fetchall()
@@ -41,13 +66,13 @@ def query_tables(table_name):
         for table_detail in data_list:
             for (k, v) in table_detail.items():
                 if table_name in v:
-                 tables.append(v)
+                    tables.append(v)
     return tables
 
 
-def query_columns(table_name):
-    sql = "select column_name from information_schema.columns  where table_name='"+table_name+"'"
-    db = getDb()
+def query_columns(database, table_name):
+    sql = "select column_name from information_schema.columns  where table_name='" + table_name + "'"
+    db = __get_db(database)
     cursor = db.cursor()
     cursor.execute(sql)
     data_list = cursor.fetchall()
@@ -59,8 +84,8 @@ def query_columns(table_name):
     return columns
 
 
-def update_execute(sql):
-    db = getDb()
+def update_execute(database, sql):
+    db = __get_db(database)
     cursor = db.cursor()
     update_count = 0
     try:
@@ -70,3 +95,60 @@ def update_execute(sql):
         db.rollback()
     db.close()
     return update_count
+
+
+def update_column(database, table_name, column_name, column_value, primary_key, primary_value):
+    sql = "update " + table_name + " set " + column_name + "= '" + column_value + "' where " + primary_key + " = '" + primary_value + "'";
+    db = __get_db(database)
+    cursor = db.cursor()
+    update_count = 0
+    try:
+        update_count = cursor.execute(sql)
+        db.commit()
+    except UnicodeEncodeError as e:
+        db.rollback()
+    db.close()
+    return update_count
+
+
+def del_by_id(database, table_name, id):
+    "根据id删除数据"
+    sql = "delete from " + table_name + " where id = " + id
+    db = __get_db(database)
+    cursor = db.cursor()
+    update_count = 0
+    try:
+        update_count = cursor.execute(sql)
+        db.commit()
+    except UnicodeEncodeError as e:
+        db.rollback()
+    db.close()
+    return update_count
+
+
+
+def extract_table_name_from_sql(sql_str):
+    # remove the /* */ comments
+    q = re.sub(r"/\*[^*]*\*+(?:[^*/][^*]*\*+)*/", "", sql_str)
+
+    # remove whole line -- and # comments
+    lines = [line for line in q.splitlines() if not re.match("^\s*(--|#)", line)]
+
+    # remove trailing -- and # comments
+    q = " ".join([re.split("--|#", line)[0] for line in lines])
+
+    # split on blanks, parens and semicolons
+    tokens = re.split(r"[\s)(;]+", q)
+
+    # scan the tokens. if we see a FROM or JOIN, we set the get_next
+    # flag, and grab the next one (unless it's SELECT).
+
+    result = set()
+    get_next = False
+    for token in tokens:
+        if get_next:
+            if token.lower() not in ["", "select"]:
+                result.add(token)
+            get_next = False
+        get_next = token.lower() in ["from", "join"]
+    return result
